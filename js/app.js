fdeb02d693d4ded26527738c9704a596d226e2fa
Terri Valentine <terrilynnvalentine@gmail.com>
2018-03-08 18:32:52 -0500

    new repo

---
+++ js/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    $(ci2 ).hide(); 
+  
+    $(submita).click(function() {
+  
+        var radio = document.getElementsByName(you need to put the surveygizmo element id here it should look like this: sgE-325920-13-122); 
+  
+          var ci2;
+  
+          if ($( ).val() == you need to put the option id here it should look something like this: 10194) {
+  
+              ci2 = “Correct!”;
+  
+          } else {
+  
+              ci2 = “incorrect. Please try again”;
+  
+          }
+  
+          $(ci2 text).val(ci2);
+  
+          $(ci-total2).text(Your answer is  + ci2);
+  
+      });
+  
+  });                       
+                          
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
