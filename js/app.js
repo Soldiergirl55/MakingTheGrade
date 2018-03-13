78deb1bb296736925ad8f87be82a03793a8750d0
Terri Valentine <terrilynnvalentine@gmail.com>
2018-03-13 14:46:00 -0400

    Save changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
 
 
-<p>Click the button to display the rank.</p>
-
-<button onclick="displayRank()">The rank is?</button>
-
-<script>
-function displayRank() {
-    document.getElementById("demo").innerHTML = Rank();
-}
-</script>
-
-<p id="demo"></p>
-
 
 
                       
