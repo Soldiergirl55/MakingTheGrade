230b23de32d5b4e9e33a6c0720ad61fad1601468
Terri Valentine <terrilynnvalentine@gmail.com>
2018-03-14 17:32:56 -0400

    Save JS

---
+++ app.js/script.js
@@ -0,0 +1,18 @@
+function handle_submission(evt) {
+    event.preventDefault(); //Stop the form from reloading the page
+    event.stopPropagation(); //Stop the form from reloading the page
+
+   //Build up our answer object
+     var answers = {    
+         'show me': get_answer('DID YOU MAKE THE GRADE?  SOLDIER')
+     }
+     //Fill in the answer
+     fill_in_answer(DID YOU MAKE THE GRADE?  SOLDIER );
+
+     var answer_div = document.querySelector('#answers');
+     answer_div.classList.add('show');
+    }
+
+//Find the form on the page and attach a handler for when it's submitted
+var form = document.querySelector('show me');
+form.addEventListener('submit', handle_submission); //Anytime the form is submitted, we want to call the function handle_submission
